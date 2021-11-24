8a64f1ad07585b310abf45522d8e772ec71a524f
FermedePommerieux <82572754+FermedePommerieux@users.noreply.github.com>
2021-11-24 19:38:00 +0100

    Update shellyAMAX.js

--- codecs/shellyAMAX.js
+++ codecs/shellyAMAX.js
@@ -1,8 +1,8 @@
 /**
-A codec to use Shellies Gen1 or Gen2 switch and sensors to control a Bosh AMAX securitySystem
+Un codec pour convertir un simple switch en control d'AMAX
 
 Place this file alongside your
-config.json file, and add the following config:
+config.json file, and add the following simple config:
        {
              
             "name": "AMAX",
@@ -12,7 +12,7 @@ config.json file, and add the following config:
             "password": "passwd",
             "type": "securitySystem",
             "codec": "ShellyAMAX.js",
-            "ShellyGen": 1,  // 1 for gen1 and 2 for gen2
+            "ShellyGen": 1,
             "AMAXswitch_ARM": "shellies/shellyuni-98CDAC25XXXX",
             "AMAXswitch_ARM_ID": 0,
             "AMAXswitch_DISARM": "shellies/shellyuni-98CDAC2XXXX",
@@ -23,6 +23,9 @@ config.json file, and add the following config:
             "AMAXsensor_TRIG": "shellies/shellyuni-98CDAC2XXXX",
             "AMAXsensor_TRIG_ID": 1,
             "AMAXsensor_TriggeredState": 0,
+            "AMAXsensor_Alt": "shellies/shellyuni-98CDAC2XXXX",
+            "AMAXsensor_Alt_ID": 1,
+            "AMAXsensor_AltState": 0,
             "targetStateValues": [
                 "SA",
                 "AA",
@@ -47,30 +50,37 @@ config.json file, and add the following config:
 
 **/
 
-
 function init( params ) {
     let { log, config, publish, notify } = params;
     let target_state=null,
         current_state=null,
-        trigger_state=null,
+        alt_trigger_state=null,
        	Switch_ARM_Topic = "/rcp",
-    	Switch_DISARM_Topic = Switch_ARM_Topic ;
+    	Switch_DISARM_Topic = Switch_ARM_Topic,
+    	ARMTopic = "/status/input:" + config.AMAXsensor_ARM_ID.toString(),
+    	TRIGTopic = "/status/input:" + config.AMAXsensor_TRIG_ID.toString();
+   // 	AltTopic="/status/input:" + config.AMAXsensor_Alt_ID; uncomment to add AltSensorState
     
-    // Topics settings, could be better coded inside the let declaration
+    // topics definition 
     if (config.ShellyGen == 1) {
     	Switch_ARM_Topic = config.AMAXswitch_ARM + "/relay/" + config.AMAXswitch_ARM_ID.toString() + "/command";
     	Switch_DISARM_Topic = config.AMAXswitch_DISARM + "/relay/" + config.AMAXswitch_DISARM_ID.toString() + "/command";
+    	ARMTopic = "/input/" + config.AMAXsensor_ARM_ID.toString();
+    	TRIGTopic = "/input/" + config.AMAXsensor_TRIG_ID.toString();
+ //   	AltTopic = "/input/" + config.AMAXsensor_Alt_ID; // uncomment to add AltSensorState
     }
     
-    let ARMTopic = "/input/" + config.AMAXsensor_ARM_ID.toString(),
-    	TRIGTopic = "/input/" + config.AMAXsensor_TRIG_ID.toString();
 
 	config.topics = {
-	"getCurrentState":config.AMAXsensor_ARM + ARMTopic,
+		"getCurrentState":config.AMAXsensor_TRIG + TRIGTopic,
         "getTargetState":config.AMAXsensor_ARM + ARMTopic,
-        "setTargetState":Switch_ARM_Topic, // Caution, two methods could exist, this only set one
-        "getAltSensorState":config.AMAXsensor_TRIG + TRIGTopic // change "getAltSensorState" to "getFaultState" if you use mqtthing 1.32
-    };
+        "setTargetState":Switch_ARM_Topic }; // Caution, two methods could exist
+    
+/* uncomment to add AltSensorState
+    if (typeof config.AMAXsensor_Alt != 'undefined' ) {
+		config.topics = Object.assign({},config.topics,"getAltSensorState":config.AMAXsensor_Alt + AltTopic);
+    }
+*/
     
     log(`Starting Bosh AMAX key switch Codec for ${config.name} with sensors:
     	ARMED : ${config.AMAXsensor_ARM + ARMTopic}
@@ -90,69 +100,62 @@ function init( params ) {
             //getTargetState return targetState
             if (msg == 0) {
             	target_state = "AA";
-		notify(config.targetState,1); // not sure it works, i may not undestand well this function	
+				notify(config.targetState,1);        	
             }
             else {
             	target_state = "D";
-          	notify(config.targetState,3); // but if it works, that's a cool way to speed up things
+          		notify(config.targetState,3);
             }
             output(target_state);
         }
-            
-        if (info.property == "currentState") {
-            if (msg == config.AMAXsensor_ARMEDState) {
-            	if (trigger_state === true ) {current_state = "T"; current_state_id = 4;}
-            	else {current_state = "AA"; current_state_id = 1;}	
-            	log(`Notifying currentState = ${current_state_id}`);
-          	notify(config.currentState,current_state_id);	
-            }
-            else {
-            	current_state = "D";
-          	notify(config.currentState,3);
-            }
-		output(current_state);
-	}
-	    
-//	Here we use the AltSensorState, if the topic is not available you could use the Tampered/FaultState instead
-        if (info.property == "getAltSensorState") {
-            if (msg == config.AMAXsensor_TriggeredState && current_state == "AA") {
-            	trigger_state = true;
-            	log("Trigger detected , notifying !!!");
-            	notify(config.currentState,4);        	
-            }
-            else {
-            	trigger_state = false;
-            	log("Trigger not detected, nothing to do");     	
-            }
-		return trigger_state;
-	}
 
-/** Use this one if you use mqtthing 1.32
-        if (info.property == "getFaultState") {
-            if (msg == config.AMAXsensor_TriggeredState && current_state == "AA") {
-            	trigger_state = true;
-            	log("Trigger detected , notifying !!!");
+        // in this case we use getCurrentState to probe triggered state
+        if (info.property == "currentState") {
+       		if (msg == config.AMAXsensor_TriggeredState || alt_trigger_state == true ) {
+				current_state = "T"; current_state_id = 4;	
+            	log(`Notifying currentState = ${current_state_id}`);
+          		notify(config.currentState,current_state_id);        	
+            }
+            else {
+            	current_state = target_state ;
+          //		notify(config.currentState,3);        	
+            }
+			output(current_state);
+		}
+
+/* uncomment to add AltSensorState
+// Here we use an alternate sensor to probe triggered state (like medical or silent trigger)
+        if (info.property == "getAltSensorState") {
+            if (msg == config.AMAXsensor_AltState) {
+            	alt_trigger_state = true;
+            	log("Alternative Trigger detected , notifying !!!");
             	notify(config.currentState,4);        	
             }
             else {
-            	trigger_state = false;
+            	alt_trigger_state = false;
             	log("Trigger not detected, nothing to do");     	
-            }
-		return trigger_state;
-	}
+            	}
+			return alt_trigger_state;
+		}
+
+*/
+/** TODO
+        if (info.property == "statusTampered") {
+
+		}
 **/
 	}
 
 	function encodeAMAX(message, info, output) {
     	log(`encoding + [${info.property}] with message [${message}]`);
         if (info.property == "targetState") {
-        	if (message == target_state ) { return undefined;} // nothing to do, things going on
+        	if (message == target_state ) { return undefined;} // RAS l'info est déja passée
         	if (message == "AA" || message == "D") {
-        		// if needed we publish the changing state
+        		// si nécessaire on publie le changement d'état
         		if (message == "AA") {
         			State = true; Switch = Switch_ARM_Topic;
         			Switch_ID = config.AMAXswitch_ARM_ID;
-        			notify(config.target_state,1); // not sure it works
+        			notify(config.target_state,1);
         			notify(config.current_state,1);
         		}
         		else {
@@ -161,17 +164,18 @@ function init( params ) {
         			notify(config.target_state,3);
         			notify(config.current_state,3);
         		}
+				// on enregistre la requête
 				target_state = message;
-				// sending a pulse, check you Shelly setting to enable auto-off if needed
+				// et on envoie un pulse unique
 				if (config.ShellyGen == 1) {
 					publish(Switch, "on");
-//					setTimeOut(publish(Switch, false),1000);
+	//				publish(Switch, false);
 				}
 				else {
         			publish(Switch, JSON.stringify({id: 123, src: 'user_1',
         				method: 'Switch.Set', params: {id: Switch_ID, on: true}}));
   //      			setTimeout(publish(Switch, JSON.stringify({id: 123, src: 'user_1',
-  //      				method: 'Switch.Set', params: {id: Switch_ID, on: false}})),1000);
+  //      				method: 'Switch.Set', params: {id: Switch_ID, on: false}})),100);
 				}
 			}
 		}
