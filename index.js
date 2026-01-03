3bea8324ab81f854a7037f169e9a9b78b7a6b092
rbswift <50788724+rbswift@users.noreply.github.com>
2026-01-03 19:30:28 +1100

    Remove underscore from accessory name
    AP-NodeJS WARNING: The accessory 'Virtual Scene Switch_0' has an invalid 'Name' characteristic ('Virtual Scene Switch_0'). Please use only alphanumeric, space, and apostrophe characters. Ensure it starts and ends with an alphabetic or numeric character, and avoid emojis. This may prevent the accessory from being added in the Home App or cause unresponsiveness.

--- index.js
+++ index.js
@@ -2366,7 +2366,7 @@ function makeThing(log, config) {
                             }
                         }
                     }
-                    let buttonSvc = new Service.StatelessProgrammableSwitch( name + "_" + i, i + 1 );
+                    let buttonSvc = new Service.StatelessProgrammableSwitch( name + " " + i, i + 1 );
                     characteristic_ProgrammableSwitchEvent(buttonSvc, 'switch' + i, buttonTopic, switchValues, restrictSwitchValues);
                     characteristic_ServiceLabelIndex( buttonSvc, i + 1 );
                     services.push(buttonSvc)
